3e422e8ec5f4416acbf31d1f2503a29ff618fb57
Trent Deckard <tdeckard2000@yahoo.com>
2023-04-24 09:01:44 -0400

    .env update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_SETTINGS_PASSWORD: process.env.NEXT_PUBLIC_SETTINGS_PASSWORD
+  }
 }
 
 module.exports = nextConfig
